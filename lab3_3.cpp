c091c31c278225c62db3c29364aa0eaf431ae6ec
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-15 17:04:12 +0700

    case 2 factor

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,10 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	unsigned short int k;
+	int k;
 	scanf("%d", &k)
 	while (k > 1)
 	{
+		if (k%a==0)
+		{
+			printf("%d\n", a);
+			k/a;
+		}
 		
 	}
 	return 0;
