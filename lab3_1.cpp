d41489476092d8d93395123e31d2833d90ce19ea
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-15 16:42:49 +0700

    add max and sum variable

--- lab3_1.cpp
+++ lab3_1.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 int main()
 {
-	unsigned short int score[5][4];
+	unsigned short int score[5][4], max = 0, sum;
 	for (int i = 0; i < 5; i++)
 	{
 		scanf("%d %d %d %d", &score[i][0], &score[i][1], &score[i][2], &score[i][3]);
+		sum = score[i][0] + score[i][1] + score[i][2] + score[i][3];
+		
 	}
 	return 0;
 }
