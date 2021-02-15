bf2cb7673418d224faef61f7f9fdaaec285c0de3
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-15 17:38:28 +0700

    fixing

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -2,16 +2,23 @@
 int main()
 {
 	int k, prime[95] = {}, nprime = 0, i, finder = 0;
-	for (i = 2; i < 1000; i++)
+	for (i = 0; i < 95; i++)
+	{
+		prime[i] = 1;
+	}
+	prime[0] = 2;
+	for (i = 1; i < 1000; i++)
 	{
 		for (finder = 0; finder <= nprime; finder++)
 		{
-			if (i % prime[i] == 0)
+			if (i % prime[finder] == 0 && prime[finder] != 1)
 			{
 				break;
 			}
+			prime[nprime] = i;
 		}
 	}
+	i = 0;
 	scanf("%d", &k);
 	while (k > 1)
 	{
