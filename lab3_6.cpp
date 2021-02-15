245ca974a498d88b6fcf3a896474348bc8f032bb
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-15 18:09:48 +0700

    extract a digit

--- lab3_6.cpp
+++ lab3_6.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main()
 {
-	int receive, pair[2] sum[10];
+	int receive, pair[2], digit[10], sum[10];
 	scanf("^f", &receive);
 	pair[0] = receive;
-	pari[1] = receive;
-	
-	return 0;
+	pair[1] = receive;
+	loop:
+		sum[0] = pair[0]/1000000000;
+		goto loop;
 }
