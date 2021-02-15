3a174a2ae2d8a0a642a38dbffa11cce0fef84a82
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-15 16:44:20 +0700

    max[contestant][score]

--- lab3_1.cpp
+++ lab3_1.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	unsigned short int score[5][4], max = 0, sum;
+	unsigned short int score[5][4], max[2] = {}, sum;
 	for (int i = 0; i < 5; i++)
 	{
 		scanf("%d %d %d %d", &score[i][0], &score[i][1], &score[i][2], &score[i][3]);
 		sum = score[i][0] + score[i][1] + score[i][2] + score[i][3];
-		if (max < sum)
+		if (max[1] < sum)
 		{
-			max = sum;
+			max[0] = i;
+			max[1] = sum;
 		}
 	}
 	return 0;
