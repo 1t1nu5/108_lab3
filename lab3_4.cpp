95b8ec5193f10b00a85fbad6ce68abbbd4217e16
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-15 19:03:48 +0700

    copy prime generator code form lab3_3

--- lab3_4.cpp
+++ lab3_4.cpp
@@ -1,6 +1,27 @@
 #include <stdio.h>
 int main()
 {
-	
+	int p[1000000] = {}, pos = 0, check = 0, n;
+	for (n = 2; n <= 1000000; n++)
+	{
+		check = 0;
+		while (true)
+		{
+			if (p[check] == 0)
+			{
+				p[check] = n;
+				break;
+			}
+			if (n % p[check] == 0)
+			{
+			
+				break;
+			}
+			else
+			{
+				check++;
+			}
+		}
+	}
 	return 0;
 }
