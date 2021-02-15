354fba92d8766785d91bf6896202b2b8b5200dd5
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-15 17:09:01 +0700

    Add more prime number

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -15,6 +15,66 @@ int main()
 			printf("3\n");
 			k /= 3;
 		}
+		else if (k%5==0)
+		{
+			printf("5\n");
+			k /= 5;
+		}
+		else if (k%7==0)
+		{
+			printf("7\n");
+			k /= 7;
+		}
+		else if (k%11==0)
+		{
+			printf("11\n");
+			k /= 11;
+		}
+		else if (k%13==0)
+		{
+			printf("13\n");
+			k /= 13;
+		}
+		else if (k%17==0)
+		{
+			printf("17\n");
+			k /= 17;
+		}
+		else if (k%19==0)
+		{
+			printf("19\n");
+			k /= 19;
+		}
+		else if (k%23==0)
+		{
+			printf("23\n");
+			k /= 23;
+		}
+		else if (k%29==0)
+		{
+			printf("29\n");
+			k /= 29;
+		}
+		else if (k%31==0)
+		{
+			printf("31\n");
+			k /= 31;
+		}
+		else if (k%37==0)
+		{
+			printf("37\n");
+			k /= 37;
+		}
+		else if (k%41==0)
+		{
+			printf("41\n");
+			k /= 41;
+		}
+		else if (k%43==0)
+		{
+			printf("43\n");
+			k /= 43;
+		}
 	}
 	return 0;
 }
