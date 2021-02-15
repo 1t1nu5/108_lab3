6abcd2c31afe9008f76800f5e04c707c2cdb5df2
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-15 16:41:28 +0700

    Hmm

--- lab3_1.cpp
+++ lab3_1.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main()
 {
-	unsigned short int score[5,25];
+	unsigned short int score[5][4];
+	for (int i = 0; i < 5; i++)
+	{
+		scanf("%d %d %d %d", &score[i][0], &score[i][1], &score[i][2], &score[i][3]);
+	}
 	return 0;
 }
