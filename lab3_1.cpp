b491f9499582fc74d9c8942475a2ba5a695686d6
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-15 16:55:44 +0700

    Try change variable

--- lab3_1.cpp
+++ lab3_1.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-	unsigned short int score[5][4], max[2] = {}, sum;
+	unsigned int score[5][4], max[2] = {}, sum;
 	for (int i = 0; i < 5; i++)
 	{
 		scanf("%d %d %d %d", &score[i][0], &score[i][1], &score[i][2], &score[i][3]);
