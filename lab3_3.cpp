13fdd5382321e600977acf61ce32e96873ab01ad
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-15 17:43:06 +0700

    fixiing fixiing fixiing

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,20 +1,17 @@
 #include <stdio.h>
 int main()
 {
-	int k, prime[95] = {}, nprime = 0, i, finder = 0;
-	for (i = 0; i < 95; i++)
-	{
-		prime[i] = 1;
-	}
+	int k, prime[95] = {}, nprime = 0, i, finder;
 	prime[0] = 2;
 	for (i = 1; i < 1000; i++)
 	{
-		for (finder = 0; finder <= nprime; finder++)
+		for (finder = 0; finder < nprime; finder++)
 		{
-			if (i % prime[finder] == 0 && prime[finder] != 1)
+			if (i % prime[finder] == 0)
 			{
 				break;
 			}
+			nprime++;
 			prime[nprime] = i;
 		}
 	}
