4f2734550bce5b1fc5964c1b1440f25329f27f5d
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-15 18:11:34 +0700

    coding...

--- lab3_6.cpp
+++ lab3_6.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main()
 {
-	int receive, pair[2], digit[10], sum[10];
+	int receive, pair[2], digit[2][10], sum[10], multiplier = 1000000000;
 	scanf("^f", &receive);
 	pair[0] = receive;
 	pair[1] = receive;
 	loop:
-		sum[0] = pair[0]/1000000000;
+		digit[0][0] = pair[0]/multiplier;
+		digit[0][1]
 		goto loop;
 }
