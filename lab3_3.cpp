f79f69c168ece5b5d4b39a5e462a6414de598b20
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-15 17:46:13 +0700

    clear and rethink

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,20 +1,10 @@
 #include <stdio.h>
 int main()
 {
-	int k, prime[95] = {}, nprime = 0, i, finder;
-	prime[0] = 2;
-	for (i = 1; i < 1000; i++)
-	{
-		for (finder = 0; finder < nprime; finder++)
-		{
-			if (i % prime[finder] == 0)
-			{
-				break;
-			}
-			nprime++;
-			prime[nprime] = i;
-		}
-	}
+	int k, prime[95] = {}, nprime = 0;
+	
+	
+	
 	i = 0;
 	scanf("%d", &k);
 	while (k > 1)
